d6ebec0c29d0b6f64c2c23d3584197ef5493ff4b
themefromroadmovie <124299688+themefromroadmovie@users.noreply.github.com>
2023-02-21 13:33:55 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,33 +1,40 @@
 const navBar = document.querySelector('.navbar')
-const navMobile = document.querySelector('.mobile')
+const navMobile = document.querySelector('.navbar--mobile')
 const navBtn = document.querySelector('.hamburger')
-const allNavItems = document.querySelectorAll('.navbar-item')
+const allNavItems = document.querySelectorAll('.navbar__item')
 const footerYear = document.querySelector('.footer__year')
 
 const handleNav = () => {
 	navBtn.classList.toggle('is-active')
-	navMobile.classList.toggle('mobile--active')
+	navMobile.classList.toggle('navbar--active')
 }
 
 allNavItems.forEach(item => {
 	item.addEventListener('click', () => {
-		navMobile.classList.remove('mobile--active')
+		navMobile.classList.remove('navbar--active')
 		navBtn.classList.remove('is-active')
 	})
 })
 
 function addShadow() {
-	if (window.scrollY >= 150) {
-		navBar.classList.add('nav-background')
+	if (window.scrollY >= 170) {
+		navBar.classList.add('navbar--background')
 	} else {
-		navBar.classList.remove('nav-background')
+		navBar.classList.remove('navbar--background')
+	}
+	if (navMobile.classList.contains('navbar--active')) {
+		setTimeout(function () {
+			navBar.classList.remove('navbar--background')
+		}, 100)
 	}
 }
 
 navBtn.addEventListener('click', handleNav)
 window.addEventListener('scroll', addShadow)
-const handleCurrentYear = () => {
-	const year = new Date().getFullYear()
-	footerYear.innerText = year
-}
-handleCurrentYear()
+window.addEventListener('click', addShadow)
+
+// const handleCurrentYear = () => {
+// 	const year = new Date().getFullYear()
+// 	footerYear.innerText = year
+// }
+// handleCurrentYear()
